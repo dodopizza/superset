614e5793432ca4182796361ea75857b184418781
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2021-06-11 17:07:50 +0300

    fix: Fix script

--- local.sh
+++ local.sh
@@ -3,7 +3,6 @@
 superset db upgrade
 
 export FLASK_APP=superset
-flask fab create-admin --username admin --firstname admin --lastname admin --email admin@example.com  --password admin 2>/dev/null
-
+flask fab create-admin --username admin --firstname admin --lastname admin --email admin@example.com --password admin 2>/dev/null
 
 superset init
