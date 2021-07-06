82fd050efe5d14c09324d001ecc14d1a507e5fa6
Eugene Bikkinin <xnegxneg@gmail.com>
2021-07-06 16:25:39 +0300

    Fix metrics labels.

--- superset/prometheus_stats_logger.py
+++ superset/prometheus_stats_logger.py
@@ -10,22 +10,24 @@ try:
         def __init__(self, prefix: str = "superset") -> None:
             super().__init__(prefix)
 
-            self._gauge = Gauge(f"{self.prefix}_gauge", f"Gauge metric for Superset",
-                                labelnames=("key"))
+            self._gauge = Gauge(f"{self.prefix}_gauge", "Gauge metric for Superset",
+                                labelnames=["key"])
+
             self._summary = Summary(f"{self.prefix}_summary",
-                                    f"Summary metric for Superset", labelnames=("key"))
+                                    f"Summary metric for Superset", labelnames=["key"])
 
         def incr(self, key: str) -> None:
-            self._gauge.labels(key).inc()
+            # self._gauge.labels()
+            self._gauge.labels(key=key).inc()
 
         def decr(self, key: str) -> None:
-            self._gauge.labels(key).dec()
+            self._gauge.labels(key=key).dec()
 
         def timing(self, key: str, value: float) -> None:
-            self._summary.labels(key).observe(value)
+            self._summary.labels(key=key).observe(value)
 
         def gauge(self, key: str, value: float) -> None:
-            self._gauge.labels(key).set(value)
+            self._gauge.labels(key=key).set(value)
 
 except Exception:  # pylint: disable=broad-except
     pass
