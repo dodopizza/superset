7b5c71a1d4320088c2bf322c3c232e92515a3e3a
Eugene Bikkinin <xnegxneg@gmail.com>
2021-09-27 15:10:21 +0300

    Remove SQLALCHEMY_ECHO.

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -97,7 +97,6 @@ WEBDRIVER_BASEURL = "http://superset:8088/"
 WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
 
 SQLLAB_CTAS_NO_LIMIT = True
-SQLALCHEMY_ECHO = True
 
 #
 # Optionally import superset_config_docker.py (which will have been included on
