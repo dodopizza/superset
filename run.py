2d56145415deaa019c08a71dd8da9a60f0e2bfb0
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2021-12-12 22:39:51 +0300

    feat: Add local debug with superset-plugins

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
+from dotenv import load_dotenv
 from superset import create_app
 
 if __name__ == "__main__":
+    load_dotenv()
     app = create_app()
     app.run(host="localhost", port=8080, debug=True)
