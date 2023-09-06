16e5c85a58353369a7c46bf8b34be06af01d333c
liamtorrelli <atleast24.7@list.ru>
2023-09-06 13:02:49 +0800

    Added dashboard id to Dash modal

--- superset-frontend/src/dashboard/components/PropertiesModal/index.tsx
+++ superset-frontend/src/dashboard/components/PropertiesModal/index.tsx
@@ -1,21 +1,5 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// DODO was here
+
 import React, { useCallback, useEffect, useState } from 'react';
 import { Input } from 'src/components/Input';
 import { FormItem } from 'src/components/Form';
@@ -542,7 +526,13 @@ const PropertiesModal = ({
       >
         <Row>
           <Col xs={24} md={24}>
-            <h3>{t('Basic information')}</h3>
+            <h3>
+              {t('Basic information')}
+              <small>
+                (id: &nbsp;
+                <b>{dashboardInfo ? dashboardInfo.id : 'Loading...'}</b>)
+              </small>
+            </h3>
           </Col>
         </Row>
         <Row gutter={16}>
