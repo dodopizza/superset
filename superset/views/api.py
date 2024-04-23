cbfd72c3d7c8275a4e41c477bc6237d0c8f0e3d9
artem <simukhin2014@mail.ru>
2024-04-23 21:00:38 +0700

    заменил декоратор с has_access_api на protect

--- superset/views/api.py
+++ superset/views/api.py
@@ -90,8 +90,7 @@ class Api(BaseSupersetView):
 
     @api
     @handle_api_exception
-    @has_access_api
-    @protect()
+    @protect
     @rison(get_time_range_schema)
     @expose("/v1/time_range/", methods=("GET",))
     def time_range(self, **kwargs: Any) -> FlaskResponse:
