a9078c365295d184eba1443e6439ba856c332285
artem <simukhin2014@mail.ru>
2024-04-25 13:56:34 +0700

    поменял метсами обертки

--- superset/views/api.py
+++ superset/views/api.py
@@ -89,6 +89,7 @@ class Api(BaseSupersetView):
         return self.json_response(form_data)
 
     @api
+    @protect
     @handle_api_exception
     @has_access_api
     @rison(get_time_range_schema)
