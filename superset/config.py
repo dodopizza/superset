e1925344ae313348148b0e31e5b89e0b43975723
“Mikhail <m.buranov@dodobrands.io>
2024-05-30 13:15:37 +0400

    готово

--- superset/config.py
+++ superset/config.py
@@ -149,7 +149,8 @@ ROW_LIMIT = 50000
 # default row limit when requesting samples from datasource in explore view
 SAMPLES_ROW_LIMIT = 1000
 # default row limit for native filters
-NATIVE_FILTER_DEFAULT_ROW_LIMIT = 1000
+# NATIVE_FILTER_DEFAULT_ROW_LIMIT = 1000 #DODO commented #34232579
+NATIVE_FILTER_DEFAULT_ROW_LIMIT = 2000 #DODO added #34232579
 # max rows retrieved by filter select auto complete
 FILTER_SELECT_ROW_LIMIT = 10000
 # default time filter in explore
