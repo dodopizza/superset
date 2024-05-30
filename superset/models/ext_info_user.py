dc857aba6d02556dcbdbbcea47d38726e316eaeb
artem <simukhin2014@mail.ru>
2024-05-30 12:43:43 +0700

    change model

--- superset/models/ext_info_user.py
+++ superset/models/ext_info_user.py
@@ -1,26 +1,36 @@
-from flask_appbuilder.security.sqla.models import User
-from sqlalchemy import Column, String, Boolean, DateTime
+from datetime import datetime
+
+from sqlalchemy import Column, String, Boolean, DateTime, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+from flask_appbuilder import Model
 
 from superset import db
-from superset.models.helpers import AuditMixinNullable
+from superset.extensions import (
+    security_manager
+)
 
 
-class DodoUser(User):
-    """
-    Extending the User Model for DodoBrands needs
-    """
+class ExtInfoUser(Model):  # pylint: disable=too-few-public-methods
 
-    __tablename__ = 'ab_user'
-    language = Column(String(32), default='en')
-    IsOnboardingFinished = Column(Boolean, default=False)
-    OnboardingStartedTime = Column(DateTime, nullable=True)
+    """Extra info about user"""
+
+    __tablename__ = "ext_info_user"
+
+    id = Column(Integer, primary_key=True)
+    isOnboardingFinished = Column(Boolean, default=False)
+    onboardingStartedTime = Column(DateTime, nullable=True)
+    language = Column(String(32), default="ru")
+    user_id = Column(Integer, ForeignKey("ab_user.id"))
+    user = relationship(
+        security_manager.user_model, backref="ext_info_user", foreign_keys=[user_id]
+    )
 
     @property
     def get_language(self):
-        return self.language or "en"
+        return self.language or "ru"
 
-    @classmethod
-    def get(cls, user_id: int):
-        db.session.get(user_id)
-        qry = db.session.get(user_id)
-        return qry.one_or_none()
+    # @classmethod
+    # def get(cls, user_id: int) -> ExtInfoUser:
+    #     db.session.get(user_id)
+    #     qry = db.session.get(user_id)
+    #     return qry.one_or_none()
