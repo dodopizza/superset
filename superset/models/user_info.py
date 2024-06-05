406954c86825460f6d5b3e733b559c10151db7f1
artem <simukhin2014@mail.ru>
2024-06-05 10:36:38 +0700

    add new fields in UserInfo for onboarding

--- superset/models/user_info.py
+++ superset/models/user_info.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, ForeignKey, Integer
+from sqlalchemy import Column, String, ForeignKey, Integer, Boolean, DateTime
 from flask_appbuilder import Model
 
 from superset import db
@@ -12,5 +12,8 @@ class UserInfo(Model):  # DODO added #33835937
     __tablename__ = "user_info"
 
     id = Column(Integer, primary_key=True)
+    isOnboardingFinished = Column(Boolean, default=False)  # DODO added #32839638
+    onboardingStartedTime = Column(DateTime)  # DODO added #32839638
     language = Column(String(32), default="ru")
     user_id = Column(Integer, ForeignKey("ab_user.id"))
+    team = Column(String(32), nullable=True)
