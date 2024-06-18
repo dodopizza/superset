c29cfa2da468cd006cb8fee4407e0e017505eb57
michael-916310 <m916310m@gmail.com>
2024-06-18 15:33:33 +0400

    fix png on 2 step

--- superset-frontend/src/DodoExtensions/onBoarding/repository/loadTeamList.repository.ts
+++ superset-frontend/src/DodoExtensions/onBoarding/repository/loadTeamList.repository.ts
@@ -8,7 +8,7 @@ export const loadTeamListRepository = async (
   query: string,
 ): Promise<Array<Team>> => {
   const url = `/api/v1/team/?isExternal=${
-    userFrom === userFromEnum.Franchisee
+    userFrom === userFromEnum.Franchisee ? 1 : 0
   }&query=${query}`;
 
   const response = await SupersetClient.get({
