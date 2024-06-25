4e038d8abf1d201900138d45883eef81b56da645
michael-916310 <m916310m@gmail.com>
2024-06-25 13:23:05 +0400

    изменил урл сингл заявки

--- superset-frontend/src/DodoExtensions/onBoarding/consts.ts
+++ superset-frontend/src/DodoExtensions/onBoarding/consts.ts
@@ -1,6 +1,6 @@
 export const ONBOARDING_LOCAL_STORAGE_KEY = 'onboarding';
 export const REQUEST_PAGE_LIST_URL = `/onboarding/request/list`;
-export const REQUEST_PAGE_URL = `/onboarding/request/edit/:id/`;
+export const REQUEST_PAGE_URL = `/onboarding/request/edit/:id`;
 
 export const MIN_TEAM_NAME_LENGTH = 3;
 export const MAX_TEAM_NAME_LENGTH = 20;
