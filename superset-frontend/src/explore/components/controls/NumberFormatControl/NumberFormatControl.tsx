ec62472e7526e9700908dd17b0a8fc0ee80a01c8
etokiryau <123979827+etokiryau@users.noreply.github.com>
2024-11-05 10:35:42 +0300

    Number format control finalized

--- superset-frontend/src/explore/components/controls/NumberFormatControl/NumberFormatControl.tsx
+++ superset-frontend/src/explore/components/controls/NumberFormatControl/NumberFormatControl.tsx
@@ -2,23 +2,22 @@
 import React from 'react';
 import { t } from '@superset-ui/core';
 import { Select } from 'src/components';
-import { SelectProps } from 'src/components/Select/types';
 import { D3_FORMAT_OPTIONS } from 'src/explore/controls';
 import ControlHeader from '../../ControlHeader';
 
 export interface NumberFormatControlProps {
+  value?: string;
   onChange: (format: string) => void;
-  currencySelectOverrideProps?: Partial<SelectProps>;
 }
 
-const options = D3_FORMAT_OPTIONS.map(option => ({
+const options = [['', t('None')], ...D3_FORMAT_OPTIONS].map(option => ({
   value: option[0],
   label: option[1],
 }));
 
 export const NumberFormatControl = ({
+  value = '',
   onChange,
-  currencySelectOverrideProps = {},
   ...props
 }: NumberFormatControlProps) => (
   <>
@@ -27,11 +26,11 @@ export const NumberFormatControl = ({
       ariaLabel={t('Number format')}
       options={options}
       placeholder={t('Number format')}
+      value={value}
       onChange={onChange}
       onClear={() => onChange('')}
       allowClear
       allowNewOptions
-      {...currencySelectOverrideProps}
     />
   </>
 );
