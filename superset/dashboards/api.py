072cea6005a9a540cdb727da31f4635d3d199e91
artemaslamov <simukhin2014@mail.ru>
2024-11-14 15:56:06 +0700

    сделал перевод стобцов

--- superset/dashboards/api.py
+++ superset/dashboards/api.py
@@ -525,9 +525,10 @@ class DashboardRestApi(BaseSupersetModelRestApi):
                             column["label"] = column.get("labelRU")
 
                     groupby = chart.get("form_data", {}).get("groupby", [])
-                    for column in groupby:
-                        if isinstance(column, dict) and column.get("labelRU"):
-                            column["label"] = column.get("labelRU")
+                    if groupby:
+                        for column in groupby:
+                            if isinstance(column, dict) and column.get("labelRU"):
+                                column["label"] = column.get("labelRU")
 
                     metrics = chart.get("form_data").get("metrics")
                     if metrics:
