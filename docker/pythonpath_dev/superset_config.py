4de4ed07e44d620777cfd598f83ec32beebdc2d2
artemaslamov <simukhin2014@mail.ru>
2024-11-19 10:47:10 +0700

    добавил проверку на возврат user_id

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -96,7 +96,9 @@ class CeleryConfig:
 
 CELERY_CONFIG = CeleryConfig
 
-FEATURE_FLAGS = {"ALERT_REPORTS": True}
+FEATURE_FLAGS = {"ALERT_REPORTS": True,
+                 "DASHBOARD_NATIVE_FILTERS_SET": True
+                 }
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"
 # The base URL for the email report hyperlinks.
