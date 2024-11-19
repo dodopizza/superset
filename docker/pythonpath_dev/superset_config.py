4398b1799818fa7b4271460df454f63a2285ab69
artemaslamov <simukhin2014@mail.ru>
2024-11-19 15:07:39 +0700

    убрал фича флаг

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -96,9 +96,7 @@ class CeleryConfig:
 
 CELERY_CONFIG = CeleryConfig
 
-FEATURE_FLAGS = {"ALERT_REPORTS": True,
-                 "DASHBOARD_NATIVE_FILTERS_SET": True
-                 }
+FEATURE_FLAGS = {"ALERT_REPORTS": True}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"
 # The base URL for the email report hyperlinks.
