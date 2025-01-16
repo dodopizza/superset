a65c7c6effc4c45323c7c3dca69ba80c53f15318
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-01-16 11:05:48 +0300

    Insertion of font awesome links into entry js file

--- superset-frontend/src/config/addCss/helpers.js
+++ superset-frontend/src/config/addCss/helpers.js
@@ -3,6 +3,31 @@ const glob = require('glob');
 const prependFile = require('prepend-file');
 
 /**
+ * Generates HTML for the Font Awesome CSS links
+ * @returns string
+ */
+const getFontAwesomeLinksScript = () => `
+(function () {
+  var head = document.head;
+  var link1 = document.createElement('link');
+  link1.rel = 'stylesheet';
+  link1.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/fontawesome.min.css';
+  link1.integrity = 'sha512-giQeaPns4lQTBMRpOOHsYnGw1tGVzbAIHUyHRgn7+6FmiEgGGjaG0T2LZJmAPMzRCl+Cug0ItQ2xDZpTmEc+CQ==';
+  link1.crossOrigin = 'anonymous';
+  link1.referrerPolicy = 'no-referrer';
+  head.appendChild(link1);
+
+  var link2 = document.createElement('link');
+  link2.rel = 'stylesheet';
+  link2.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/solid.min.css';
+  link2.integrity = 'sha512-6mc0R607di/biCutMUtU9K7NtNewiGQzrvWX4bWTeqmljZdJrwYvKJtnhgR+Ryvj+NRJ8+NnnCM/biGqMe/iRA==';
+  link2.crossOrigin = 'anonymous';
+  link2.referrerPolicy = 'no-referrer';
+  head.appendChild(link2);
+})();
+`;
+
+/**
  *
  * @param {*} hash - hash of the file
  * @param {*} path - public path
@@ -45,7 +70,10 @@ const updateFileWithCss = (outputFolder, name, path) => {
       console.log('Files to update:', JSON.stringify(files));
       const hash = files[0].split(`${name}.`)[1].split('.js')[0];
       const scriptText = getScriptText(hash, name, path);
-      prependFile(files[0], scriptText);
+      const fontAwesomeLinks = getFontAwesomeLinksScript();
+      const combinedText = fontAwesomeLinks + scriptText;
+
+      prependFile(files[0], combinedText);
       console.log(
         'Files were updated with script:\n\n',
         JSON.stringify(scriptText.split('\n').join('').split('  ').join(' ')),
