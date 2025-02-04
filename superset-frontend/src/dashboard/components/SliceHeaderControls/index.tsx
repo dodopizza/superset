9c8f6462290e23d725448bdf899967f0c7a5279b
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-02-04 13:53:48 +0300

    refactor: unnecessary changes removed

--- superset-frontend/src/dashboard/components/SliceHeaderControls/index.tsx
+++ superset-frontend/src/dashboard/components/SliceHeaderControls/index.tsx
@@ -1,4 +1,21 @@
-// DODO was here
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 import {
   MouseEvent,
   Key,
@@ -516,7 +533,7 @@ const SliceHeaderControls = (props: SliceHeaderControlsPropsWithRouter) => {
   const [openScopingModal, scopingModal] = useCrossFiltersScopingModal(
     props.slice.slice_id,
   );
-  // const history = useHistory(); // DODO commented out 44120742
+  const history = useHistory();
 
   const queryMenuRef: RefObject<any> = useRef(null);
   const menuRef: RefObject<any> = useRef(null);
@@ -585,8 +602,7 @@ const SliceHeaderControls = (props: SliceHeaderControlsPropsWithRouter) => {
           domEvent.preventDefault();
           window.open(props.exploreUrl, '_blank');
         } else {
-          // history.push(props.exploreUrl);
-          window.open(props.exploreUrl, '_blank'); // DODO changed 44120742
+          history.push(props.exploreUrl);
         }
         break;
       case MenuKeys.ExportCsv:
