d4059ab331751cca10e116867b2f7c805fe5947a
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-02-04 16:44:53 +0300

    chore: comments for resize observer

--- superset-frontend/src/components/AsyncAceEditor/index.tsx
+++ superset-frontend/src/components/AsyncAceEditor/index.tsx
@@ -1,21 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// DODO was here
 import { forwardRef, useEffect, ComponentType } from 'react';
 
 import {
@@ -87,7 +70,7 @@ const aceModuleLoaders = {
 export type AceModule = keyof typeof aceModuleLoaders;
 
 type AsyncAceEditorPropsDodoExtended = {
-  hasVerticalResize?: boolean;
+  hasVerticalResize?: boolean; // DODO added 45047288
 };
 export type AsyncAceEditorProps = IAceEditorProps & {
   keywords?: AceCompleterKeyword[];
@@ -138,8 +121,8 @@ export default function AsyncAceEditor(
           theme = inferredTheme,
           tabSize = defaultTabSize,
           defaultValue = '',
-          maxLines,
-          hasVerticalResize,
+          maxLines, // DODO added 45047288
+          hasVerticalResize, // DODO added 45047288
           ...props
         },
         ref,
@@ -234,7 +217,7 @@ export default function AsyncAceEditor(
               tabSize={tabSize}
               defaultValue={defaultValue}
               setOptions={{ fontFamily }}
-              maxLines={hasVerticalResize ? undefined : maxLines}
+              maxLines={hasVerticalResize ? undefined : maxLines} // DODO added 45047288
               {...props}
             />
           </>
