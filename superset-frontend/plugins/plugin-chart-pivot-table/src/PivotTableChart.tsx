f9f14682f9f2b4fb3c4e055b577012ae54e53c6e
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-02-19 16:54:54 +0300

    feat: pivot viz

--- superset-frontend/plugins/plugin-chart-pivot-table/src/PivotTableChart.tsx
+++ superset-frontend/plugins/plugin-chart-pivot-table/src/PivotTableChart.tsx
@@ -1,21 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// DODO was here
 import { useCallback, useMemo } from 'react';
 import { MinusSquareOutlined, PlusSquareOutlined } from '@ant-design/icons';
 import {
@@ -155,6 +138,8 @@ export default function PivotTableChart(props: PivotTableProps) {
     dateFormatters,
     onContextMenu,
     timeGrainSqla,
+    columnConfig, // DODO added 45525377
+    pinnedColumns, // DODO added 45525377
   } = props;
 
   const theme = useTheme();
@@ -556,6 +541,11 @@ export default function PivotTableChart(props: PivotTableProps) {
           subtotalOptions={subtotalOptions}
           namesMapping={verboseMap}
           onContextMenu={handleContextMenu}
+          // DODO added start 45525377
+          columnConfig={columnConfig}
+          combineMetric={combineMetric}
+          pinnedColumns={pinnedColumns}
+          // DODO added stop 45525377
         />
       </PivotTableWrapper>
     </Styles>
