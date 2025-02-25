2aabf31504980caa40c9bd8f80117134fcb00b99
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-02-25 14:15:26 +0300

    refactor: rollbar checkIgnore func

--- superset-frontend/src/firebase/rollbar.ts
+++ superset-frontend/src/firebase/rollbar.ts
@@ -1,7 +1,7 @@
+import type { Configuration } from 'rollbar';
 import { APP_VERSION } from 'src/constants';
 
 type Env = 'production' | 'development' | 'local';
-
 const ENV_MAP: Record<string, Env> = {
   'analytics.dodois.': 'production',
   'officemanager.dodopizza.': 'production',
@@ -10,6 +10,11 @@ const ENV_MAP: Record<string, Env> = {
   'superset.d.yandex.dodois.': 'development',
   'spr.d.yandex.dodois.': 'development',
   'localhost.': 'local',
+};
+
+const ERROR_WHITE_LIST: Record<string, string> = {
+  'ResizeObserver loop completed with undelivered notifications.': 'true',
+  'ajax is not defined': 'true',
 };
 
 const isPlugin = process.env.type !== undefined;
@@ -34,8 +39,8 @@ const getEnv = (): string => {
   return env;
 };
 
-export const ROLLBAR_CONFIG = {
-  accessToken: '174d57ae38994b0f9af404174d983082',
+export const ROLLBAR_CONFIG: Configuration = {
+  accessToken: 'd9021ea67e624bcc904ff9deae004565',
   captureUncaught: true,
   captureUnhandledRejections: true,
   payload: {
@@ -47,4 +52,27 @@ export const ROLLBAR_CONFIG = {
     },
   },
   version: APP_VERSION,
+  checkIgnore: (isUncaught, args, payload: RollbarPayload) => {
+    const description = payload?.body?.trace?.exception?.description || '';
+    const message = payload?.body?.trace?.exception?.message || '';
+
+    return Boolean(ERROR_WHITE_LIST[description] || ERROR_WHITE_LIST[message]);
+  },
 };
+
+interface RollbarException {
+  description?: string;
+  message?: string;
+}
+
+interface RollbarTrace {
+  exception?: RollbarException;
+}
+
+interface RollbarBody {
+  trace?: RollbarTrace;
+}
+
+interface RollbarPayload {
+  body?: RollbarBody;
+}
