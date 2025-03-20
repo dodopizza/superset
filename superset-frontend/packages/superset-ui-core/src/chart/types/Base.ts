75be2ad34f0e3b3f85d32a8445109401bf43e0ef
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-03-20 16:35:43 +0300

    new strings

--- superset-frontend/packages/superset-ui-core/src/chart/types/Base.ts
+++ superset-frontend/packages/superset-ui-core/src/chart/types/Base.ts
@@ -1,24 +1,7 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// DODO was here
 
 import { BinaryQueryObjectFilterClause, ExtraFormData } from '../../query';
-import { JsonObject } from '../..';
+import { JsonObject, t } from '../..';
 
 export type HandlerFunction = (...args: unknown[]) => void;
 
@@ -78,10 +61,14 @@ export enum ChartLabel {
 }
 
 export const chartLabelExplanations: Record<ChartLabel, string> = {
-  [ChartLabel.Deprecated]:
+  // DODO changed 44612143
+  [ChartLabel.Deprecated]: t(
     'This chart uses features or modules which are no longer actively maintained. It will eventually be replaced or removed.',
-  [ChartLabel.Featured]:
+  ),
+  // DODO changed 44612143
+  [ChartLabel.Featured]: t(
     'This chart was tested and verified, so the overall experience should be stable.',
+  ),
 };
 
 export const chartLabelWeight: Record<ChartLabel, { weight: number }> = {
