f587803b6a2f0af1ad576da0509c49af34ee2db9
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-03-20 16:00:25 +0300

    fix: save dataset button width in sql lab

--- superset-frontend/src/SqlLab/components/SaveDatasetActionButton/index.tsx
+++ superset-frontend/src/SqlLab/components/SaveDatasetActionButton/index.tsx
@@ -1,21 +1,4 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// DODO was here
 import { FC } from 'react';
 import { t, useTheme, styled } from '@superset-ui/core';
 import Icons from 'src/components/Icons';
@@ -38,9 +21,10 @@ const SaveDatasetActionButton = ({
     DropdownButton as FC<DropdownButtonProps>,
   )`
     &.ant-dropdown-button button.ant-btn.ant-btn-default {
-      &:first-of-type {
-        width: ${theme.gridUnit * 16}px;
-      }
+      // DODO commented out
+      // &:first-of-type {
+      //   width: ${theme.gridUnit * 16}px;
+      // }
       font-weight: ${theme.gridUnit * 150};
       background-color: ${theme.colors.primary.light4};
       color: ${theme.colors.primary.dark1};
@@ -61,7 +45,7 @@ const SaveDatasetActionButton = ({
     <Button
       onClick={() => setShowSave(true)}
       buttonStyle="primary"
-      css={{ width: theme.gridUnit * 25 }}
+      // css={{ width: theme.gridUnit * 25 }} // DODO commented out
     >
       {t('Save')}
     </Button>
