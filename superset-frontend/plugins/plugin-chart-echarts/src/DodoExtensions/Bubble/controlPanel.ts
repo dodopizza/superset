0d06ad9ce2b248af393f1c70b66e147ed35bc598
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-04-15 11:28:39 +0300

    feat: Dodo Bubble chart

---
+++ superset-frontend/plugins/plugin-chart-echarts/src/DodoExtensions/Bubble/controlPanel.ts
@@ -0,0 +1,330 @@
+import { SMART_DATE_ID, t } from '@superset-ui/core';
+import {
+  ControlPanelConfig,
+  D3_FORMAT_DOCS,
+  D3_TIME_FORMAT_OPTIONS,
+  formatSelectOptions,
+} from '@superset-ui/chart-controls';
+
+const xAxis = {
+  label: t('X Axis'),
+  expanded: false,
+  controlSetRows: [
+    [
+      {
+        name: 'x_log_scale',
+        config: {
+          type: 'CheckboxControl',
+          label: t('X Log Scale'),
+          renderTrigger: true,
+          default: false,
+          description: t('Use a log scale for the X-axis.'),
+        },
+      },
+    ],
+    [
+      {
+        name: 'x_force_timestamp_formatting',
+        config: {
+          type: 'CheckboxControl',
+          label: t('Force date format'),
+          renderTrigger: true,
+          default: false,
+          description: t(
+            'Use date formatting even when metric value is not a timestamp',
+          ),
+        },
+      },
+    ],
+    [
+      {
+        name: 'x_time_format',
+        config: {
+          type: 'SelectControl',
+          freeForm: true,
+          label: t('Date format'),
+          renderTrigger: true,
+          choices: D3_TIME_FORMAT_OPTIONS,
+          description: D3_FORMAT_DOCS,
+          default: SMART_DATE_ID,
+        },
+      },
+    ],
+    ['x_axis_format'],
+    [
+      {
+        name: 'x_axis_name',
+        config: {
+          type: 'TextControl',
+          label: t('X Axis name'),
+          renderTrigger: true,
+          default: 0,
+          description: t('X axis name'),
+        },
+      },
+    ],
+    [
+      {
+        name: 'x_name_location',
+        config: {
+          type: 'SelectControl',
+          label: t('Name location'),
+          renderTrigger: true,
+          default: 'center',
+          choices: formatSelectOptions<string>(['start', 'center', 'end']),
+        },
+      },
+      {
+        name: 'x_name_gap_in_pixel',
+        config: {
+          type: 'TextControl',
+          label: t('Name gap (in pixels)'),
+          renderTrigger: true,
+          default: 30,
+          description: t('Name gap from chart grid'),
+        },
+      },
+    ],
+  ],
+};
+
+const yAxis = {
+  label: t('Y Axis'),
+  expanded: false,
+  controlSetRows: [
+    [
+      {
+        name: 'y_log_scale',
+        config: {
+          type: 'CheckboxControl',
+          label: t('Y Log Scale'),
+          renderTrigger: true,
+          default: false,
+          description: t('Use a log scale for the Y-axis.'),
+        },
+      },
+    ],
+    [
+      {
+        name: 'y_force_timestamp_formatting',
+        config: {
+          type: 'CheckboxControl',
+          label: t('Force date format'),
+          renderTrigger: true,
+          default: false,
+          description: t(
+            'Use date formatting even when metric value is not a timestamp',
+          ),
+        },
+      },
+    ],
+    [
+      {
+        name: 'y_time_format',
+        config: {
+          type: 'SelectControl',
+          freeForm: true,
+          label: t('Date format'),
+          renderTrigger: true,
+          choices: D3_TIME_FORMAT_OPTIONS,
+          description: D3_FORMAT_DOCS,
+          default: SMART_DATE_ID,
+        },
+      },
+    ],
+    ['y_axis_format'],
+    [
+      {
+        name: 'y_axis_name',
+        config: {
+          type: 'TextControl',
+          label: t('Y Axis name'),
+          renderTrigger: true,
+          default: 0,
+          description: t('Y axis name'),
+        },
+      },
+    ],
+    [
+      {
+        name: 'y_name_location',
+        config: {
+          type: 'SelectControl',
+          label: t('Name location'),
+          renderTrigger: true,
+          default: 'center',
+          choices: formatSelectOptions<string>(['start', 'center', 'end']),
+        },
+      },
+      {
+        name: 'y_name_gap_in_pixel',
+        config: {
+          type: 'TextControl',
+          label: t('Name gap (in pixels)'),
+          renderTrigger: true,
+          default: 30,
+          description: t('Name gap from chart grid'),
+        },
+      },
+    ],
+  ],
+};
+
+const dimentions = {
+  label: t('Dimension Options'),
+  expanded: false,
+  controlSetRows: [
+    [
+      {
+        name: 'show_dimension',
+        config: {
+          type: 'CheckboxControl',
+          label: t('Show dimension'),
+          renderTrigger: true,
+          default: false,
+          description: t('Whether to display the dimension.'),
+        },
+      },
+    ],
+    [
+      {
+        name: 'margin_top_in_pixel',
+        config: {
+          type: 'TextControl',
+          label: t('Grid margin top (in pixels)'),
+          renderTrigger: true,
+          default: 0,
+          description: t('Margin top for chart grid'),
+        },
+      },
+    ],
+    [
+      {
+        name: 'scroll_dimensions',
+        config: {
+          type: 'CheckboxControl',
+          label: t('Scroll dimension'),
+          renderTrigger: true,
+          default: false,
+          description: t('Whether to scroll dimensions.'),
+        },
+      },
+    ],
+  ],
+};
+
+const labels = {
+  label: t('Label Options'),
+  expanded: true,
+  controlSetRows: [
+    [
+      {
+        name: 'show_labels',
+        config: {
+          type: 'CheckboxControl',
+          label: t('Show Labels'),
+          renderTrigger: true,
+          default: false,
+          description: t('Whether to display the labels.'),
+        },
+      },
+    ],
+    [
+      {
+        name: 'label_location',
+        config: {
+          type: 'SelectControl',
+          label: t('Label location'),
+          renderTrigger: true,
+          default: 'top',
+          choices: formatSelectOptions<string>([
+            'top',
+            'left',
+            'right',
+            'bottom',
+            'inside',
+            'insideLeft',
+            'insideRight',
+            'insideTop',
+            'insideBottom',
+            'insideTopLeft',
+            'insideBottomLeft',
+            'insideTopRight',
+            'insideBottomRight',
+          ]),
+        },
+      },
+    ],
+    [
+      {
+        name: 'label_font_size',
+        config: {
+          type: 'TextControl',
+          label: t('Label font (in pixels)'),
+          renderTrigger: true,
+          default: 12,
+        },
+      },
+    ],
+    [
+      {
+        name: 'label_color',
+        config: {
+          label: t('Label color'),
+          type: 'ColorPickerControl',
+          renderTrigger: true,
+        },
+      },
+    ],
+  ],
+};
+
+const config: ControlPanelConfig = {
+  controlPanelSections: [
+    {
+      label: t('Query'),
+      expanded: true,
+      controlSetRows: [
+        ['series'],
+        ['entity'],
+        ['x'],
+        ['y'],
+        ['adhoc_filters'],
+        ['size'],
+        [
+          {
+            name: 'max_bubble_size',
+            config: {
+              type: 'SelectControl',
+              freeForm: true,
+              label: t('Max Bubble Size'),
+              default: '25',
+              choices: formatSelectOptions([
+                '5',
+                '10',
+                '15',
+                '25',
+                '50',
+                '75',
+                '100',
+              ]),
+            },
+          },
+        ],
+      ],
+    },
+    { ...labels },
+    { ...dimentions },
+    // @ts-ignore
+    { ...xAxis },
+    // @ts-ignore
+    { ...yAxis },
+    {
+      label: t('Size'),
+      expanded: false,
+      controlSetRows: [['size_format']],
+    },
+  ],
+};
+
+export default config;
--- superset-frontend/plugins/plugin-chart-echarts/src/DodoExtensions/Bubble/index.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/DodoExtensions/Bubble/index.ts
@@ -1,46 +1,36 @@
-import { ChartMetadata, ChartPlugin, t } from '@superset-ui/core';
-import thumbnail from '../../Bubble/images/thumbnail.png';
-import transformProps from '../../Bubble/transformProps';
-import buildQuery from '../../Bubble/buildQuery';
-import controlPanel from '../../Bubble/controlPanel';
-import example1 from '../../Bubble/images/example1.png';
-import example2 from '../../Bubble/images/example2.png';
-import {
-  EchartsBubbleChartProps,
-  EchartsBubbleFormData,
-} from '../../Bubble/types';
+import { ChartLabel, ChartMetadata, ChartPlugin, t } from '@superset-ui/core';
+import controlPanel from './controlPanel';
+import thumbnail from './images/thumbnail.png';
+import transformProps from './transformProps';
+import buildQuery from './buildQuery';
 
-// TODO: Implement cross filtering
-export default class EchartsBubbleChartDodoPlugin extends ChartPlugin<
-  EchartsBubbleFormData,
-  EchartsBubbleChartProps
-> {
+const metadata = new ChartMetadata({
+  category: t('Correlation'),
+  credits: ['https://echarts.apache.org'],
+  description: t(
+    'Visualizes a metric across three dimensions of data in a single chart (X axis, Y axis, and bubble size). Bubbles from the same group can be showcased using bubble color.',
+  ),
+  name: 'EChart Bubble Chart',
+  tags: [
+    t('Multi-Dimensions'),
+    t('Scatter'),
+    t('Time'),
+    t('Trend'),
+    t('ECharts'),
+  ],
+  thumbnail,
+  deprecated: true,
+  label: ChartLabel.Deprecated,
+});
+
+export default class EchartsBubbleChartDodoPlugin extends ChartPlugin {
   constructor() {
     super({
+      loadChart: () => import('./BubbleDodo'),
+      metadata,
       buildQuery,
-      controlPanel,
-      loadChart: () => import('../../Bubble/EchartsBubble'),
-      metadata: new ChartMetadata({
-        category: t('Correlation'),
-        credits: ['https://echarts.apache.org'],
-        description: t(
-          'Visualizes a metric across three dimensions of data in a single chart (X axis, Y axis, and bubble size). Bubbles from the same group can be showcased using bubble color.',
-        ),
-        exampleGallery: [{ url: example1 }, { url: example2 }],
-        name: `${t('Bubble Chart')} Dodo`,
-        tags: [
-          t('Multi-Dimensions'),
-          t('Comparison'),
-          t('Scatter'),
-          t('Time'),
-          t('Trend'),
-          t('ECharts'),
-          t('Featured'),
-        ],
-        thumbnail,
-        deprecated: true,
-      }),
       transformProps,
+      controlPanel,
     });
   }
 }
