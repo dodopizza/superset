bc346f9817bf4c3e84066447806a46482394e482
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-04-18 12:50:49 +0300

    refactor(plugin): remove dashboard put endpoint

--- superset-frontend/src/dashboard/actions/dashboardState.js
+++ superset-frontend/src/dashboard/actions/dashboardState.js
@@ -717,12 +717,7 @@ const storeDashboardMetadata = async (id, metadata) =>
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ json_metadata: JSON.stringify(metadata) }),
       })
-    : API_HANDLER.SupersetClient({
-        method: 'put',
-        url: `/api/v1/dashboard/${id}`,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ json_metadata: JSON.stringify(metadata) }),
-      });
+    : null;
 
 /**
  *
