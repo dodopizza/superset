ec4692cf828ce7b3e76dfc8f99646421a533f656
etokiryau <123979827+etokiryau@users.noreply.github.com>
2025-05-22 10:41:17 +0300

    revert unnecessary changes

--- superset-frontend/plugins/plugin-chart-handlebars/src/components/CodeEditor/CodeEditor.tsx
+++ superset-frontend/plugins/plugin-chart-handlebars/src/components/CodeEditor/CodeEditor.tsx
@@ -1,4 +1,21 @@
-// DODO was here
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 import { FC } from 'react';
 import AceEditor, { IAceEditorProps } from 'react-ace';
@@ -9,7 +26,7 @@ import 'ace-builds/src-min-noconflict/mode-css';
 import 'ace-builds/src-noconflict/theme-github';
 import 'ace-builds/src-noconflict/theme-monokai';
 
-export type CodeEditorMode = 'handlebars' | 'css' | 'javascript'; // DODO changed
+export type CodeEditorMode = 'handlebars' | 'css';
 export type CodeEditorTheme = 'light' | 'dark';
 
 export interface CodeEditorProps extends IAceEditorProps {
